7ac24edd195688dd15074f3a6d8ec6fe6b5a7739
Brianna Hickerson <briannahickerson@Briannas-MacBook-Pro.local>
2022-09-24 23:58:23 -0700

    Finished assignments

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,18 +1,21 @@
 const prompt = require('prompt-sync')({sigint: true});
 
-let number = Number(prompt("Please enter your number: "));
-console.log(number);
+let player1 = Number(prompt("Please enter rock, paper or scissors: "));
 
 //use least common multiple in order to run the number that is divisible by 3 and 5 
 // to print. Do this first then do the 3 and 5.
 
-if (number % 15 == 0){
-    console.log("fizz buzz");
+if (player1 === 'rock' && player2 === 'paper'){
+    console.log("Player 1 is the winner!");
+
+} else if (player1 === 'paper' && player2 === 'rock'){
+    console.log("Player 2 is the winner!");
+    
+} else if (player1 === 'rock' && player2 === 'paper'){
+    console.log("Player 1 is the winner!");
 } 
-else if (number % 3 == 0){
-    console.log("fizz")
-}
- else if (number % 5 == 0){
-    console.log("buzz")
-}
+if (player1 === 'rock' && player2 === 'paper'){
+    console.log("Player 1 is the winner!");
+} 
+
 
--- weekday.js
+++ weekday.js
@@ -8,15 +8,15 @@ if(day === 1){
 } else if (day === 2){
     console.log("Today is Tuesday");
 }else if (day === 3){
-    console.log("Today is Tuesday");
+    console.log("Today is Wednesday");
 } else if (day === 4){
-    console.log("Today is Tuesday");
+    console.log("Today is Thursday");
 }else if (day === 5){
-    console.log("Today is Tuesday");
+    console.log("Today is Friday");
 }else if (day === 6){
-    console.log("Today is Tuesday");
+    console.log("Today is Saturday");
 }else if (day === 7){
-    console.log("Today is Tuesday");
+    console.log("Today is Sunday");
 } else {
     console.log("Error");
 } 
